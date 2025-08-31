76e804db249c4fd2851d4916727d99eb999e341c
eunhye <dmsgp2627@naver.com>
2025-08-31 17:04:23 +0900

    [FE]로그인 / 회원가입 / 메인페이지 완성

---
+++ frontend/ai-interview/src/component/inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useRef } from "react";
+import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
+
+const ProfilePhotoSelector = ({ image, setImage, preview, setPreview }) => {
+  //이미지를 가져오자!
+
+  const inputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      //업데이트 이미지를 저장할
+
+      setImage(file);
+
+      const priview = URL.createObjectURL(file);
+      if (setPreview) {
+        setPreview(priview);
+      }
+      setPreviewUrl(priview);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+    setPreviewUrl(null);
+
+    if (setPreview) {
+      setPreview(null);
+    }
+  };
+
+  const onChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className="flex justify-center mb-10">
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
+
+      {/*img 업로드 버튼 설정하기*/}
+      {!image ? (
+        <div className="w-20 h-20 flex items-center justify-center bg-orange-50 rounded-full relative cursor-pointer">
+          <LuUser className="text-4xl text-orange-500" />
+
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-linear-to-r from-orange-500/85 to-orange-600 text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer"
+            onClick={onChooseFile}
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        <div className="relative">
+          <img
+            src={preview || previewUrl}
+            alt="profile photo"
+            className="w-20 h-20 rounded-full object-cover"
+          />
+
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer"
+            onClick={handleRemoveImage}
+          >
+            <LuTrash />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePhotoSelector;
--- frontend/ai-interview/src/pages/Auth/SignUp.jsx
+++ frontend/ai-interview/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../component/inputs/Input";
+import ProfilePhotoSelector from "../../component/inputs/ProfilePhotoSelector";
 
-const SignUp = () => {
+
+  //입력받고 셋팅할 변수만들기
+  // 프로필, 이름, 이메일, 비밀번호
+const SignUp = ({ setCurrentPage }) => {
+
+  const [profilePic, setProfilePic] = useState(null); 
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  //회원가입 폼 만들기
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="w-[90vw] md:w-[33vw] p-12 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black ">계정 생성하기</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-8">아래 정보를 입력하고 지금 바로 가입하세요</p>
 
-export default SignUp
+      <form onSubmit={handleSignUp}>
+
+        <ProfilePhotoSelector image = {profilePic} setImage={setProfilePic} />
+
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2 mb-5">
+          <Input 
+            value={fullName}
+            onChange={({ target }) => setFullName(target.value)}
+            label="이름"
+            placeholder="이름을 입력하세요"
+            type="text"
+          />
+
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="이메일 주소"
+            placeholder="이메일을 입력하세요"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="비밀번호"
+            placeholder="최소 8자리 이상 입력하세요"
+            type="password"
+          />
+        </div>
+
+        {error && <p className="mt-2 text-sm text-red-500 pb-2.5">{error}</p>}
+
+        <button type="submit" className="btn-primary">
+          SIGN UP
+        </button>
+
+        <p className="text-[13px] text-slate-800 mt-3">
+          이미 가입하셨나요? {" "}
+          <button
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => {
+              setCurrentPage("login");
+            }}
+          >
+            LOG IN
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
