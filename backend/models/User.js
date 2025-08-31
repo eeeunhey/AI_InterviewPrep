1e23cc931aa89f07462447c084e50d89b0e01096
eunhye <dmsgp2627@naver.com>
2025-08-31 19:18:53 +0900

    STUDY_log 정리

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
-const {
-  default: ProfilePhotoSelector,
-} = require("../../frontend/ai-interview/src/component/inputs/ProfilePhotoSelector");
 
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, default: null },
-    ProfileImageUrl: { type: String, default: null },
+    profileImageUrl: { type: String, default: null }, // camelCase 권장
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
-
