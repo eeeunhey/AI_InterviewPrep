e2a93cd943c7d430058c53acf3713bf04488dfa5
eunhye <dmsgp2627@naver.com>
2025-09-06 10:03:24 +0900

    [FE]Dashboard 구현_Navbar Header add

--- frontend/ai-interview/src/pages/Home/Dashboard.jsx
+++ frontend/ai-interview/src/pages/Home/Dashboard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { LuPlus } from "react-icons/lu";
+import { CARD_BG } from "../../utils/data";
+import toast from "react-hot-toast";
+import DashboardLayout from "../../component/layout/DashboardLayout";
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   return (
-    <div>Dashboard</div>
+    <DashboardLayout>Dashboard</DashboardLayout>
   )
 }
 
